989be0f56480f713e9fc07bc76e02f1b3dab2f83
jtsherwood11 <jtsherwood11@users.noreply.github.com>
2017-02-24 05:57:11 -0700

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/*
+ * THIS SCRIPT IS TO VALIDATE EMAIL ADDRESS ACUTUALLY EXISTSSSSSS
+ * IN THEORY_>>>>>>>>>>>>>>>>>>>>>>>
+*/
+
+
+  $myFile = "list.txt";
+  $lines = file($myFile);
+  echo "<table>";
+  function domain_exists($email, $record = 'MX'){
+    list($user, $domain) = explode('@', $email);
+    return checkdnsrr($domain, $record);
+  }
+  foreach($lines as $line) {
+    $email = trim($line);
+    echo "<tr>";
+    echo "<td>$line</td>";
+    if(domain_exists($email)) {
+      echo "<td>This MX records exists; I will accept this email as valid.</td>";
+    }
+    else {
+      echo "<td>No MX record exists;  Invalid email.</td>";
+    }
+    echo "</tr>";
+ }
+
+ echo "</table>";
+
+?>
